68c250471d94d9d13444b2b149e79fd29eb23bd5
Mateo Algañarás <tutealga@gmail.com>
2023-10-25 17:03:50 -0300

    Change from Firebase to Google Sheets only

--- services/fetchDocsPrueba.js
+++ services/fetchDocsPrueba.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import Papa from 'papaparse';
 
 export const getProducts = async ({params}) => {
-    const res = await fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vQZfjAXicpEz5M5WMW724OIvZcrs-YWOpuH75QXR2_lBpjdr6UTkcx1i8Q4Lc5ypAUrkP1-ahqiYTFZ/pub?gid=0&output=csv')
+    const res = await fetch(process.env.GOOGLE_SHEETS)
     const data = await res.text();
     const parsed = await new Promise((resolve, reject) => {
         Papa.parse(data, {header:true,
