a501f809f502a435c66f9ced9dbc87d440fb2ea0
Mateo Algañarás <tutealga@gmail.com>
2023-12-21 00:26:13 -0300

    Add revalidate on demand

--- app/revalidate/route.js
+++ app/revalidate/route.js
@@ -3,7 +3,7 @@ import { revalidatePath } from "next/cache";
 export async function GET(request) {
     const searchParams = request.nextUrl.searchParams;
 
-    if(searchParams.get("secret") !== process.env.SECRET){
+    if(searchParams.get("secret") !== process.env.SECRET_TOKEN){
         return Response.json({revalidate: false});
     }
 
