033028e8278d9cbefc6a08cd97b3d7d7b6fd8c64
Mateo Algañarás <tutealga@gmail.com>
2024-05-28 00:55:44 -0300

    Style changes

--- components/Product.js
+++ components/Product.js
@@ -1,6 +1,6 @@
 const Product = ({product}) => {
   return (
-    <div style={{backgroundImage: `url(${product.img})`}} className="w-[200px] place-self-center bg-center h-[250px] bg-no-repeat p-4 bg-cover rounded-md grid items-end bg-[#131311]">
+    <div style={{backgroundImage: `url(${product.img})`}} className="w-full place-self-center bg-center h-[250px] bg-no-repeat p-4 bg-cover rounded-md grid items-end bg-[#131311]">
         <div className="flex flex-col text-white">
          <h3 className="font-bold">{product.name}</h3>
         </div>
