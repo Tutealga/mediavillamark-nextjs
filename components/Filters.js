49e58116682190aaed82f0c66994978a8e147f99
Mateo Algañarás <tutealga@gmail.com>
2024-05-28 20:31:29 -0300

    Style changes

--- components/Filters.js
+++ components/Filters.js
@@ -50,8 +50,8 @@ router.push(newUrl, { scroll: false })
         onClick={()=> handleFilter(link)}
         className={`${
             active === link ? 
-            'bg-white text-red-600' : ''
-        } whitespace-nowrap rounded-md px-2 py-1 capitalize border-red-600 bg-red-600 text-white border hover:bg-white hover:text-red-600`}
+            'bg-white text-red-600' : 'text-white'
+        } whitespace-nowrap rounded-md px-2 py-1 capitalize border-red-600 bg-red-600 border hover:bg-white hover:text-red-600`}
         >
 {link.name}
         </button>
